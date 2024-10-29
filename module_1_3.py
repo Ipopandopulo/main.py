b0df77dc159d26df674c25beb6ca399c3277fd42
Ipopandopulo <148614488+Ipopandopulo@users.noreply.github.com>
2024-10-29 19:21:30 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = 'Илья'
+print('Name: ' + name)
+
+age =  33
+print('Age: ' + str(age))
+
+new_age = 34
+print('New_age: ' + str(new_age))
+
+is_student = True
+print('Is Student: ' + str(is_student))
+
