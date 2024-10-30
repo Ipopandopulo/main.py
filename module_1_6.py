3b4442393a66beebd587787d07d6f00a0202fac2
Ipopandopulo <148614488+Ipopandopulo@users.noreply.github.com>
2024-10-30 08:47:02 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,6 @@
+number_hours = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_task = hours_spent / number_hours
+
+print('Курс:', course_name,', '  'всего задач:' ,number_hours, ', ' 'среднее время выполнения:', time_task, 'часа.' )
